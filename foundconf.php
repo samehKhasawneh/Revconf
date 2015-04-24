9d6d951bf050827782ec62e76805cfc8fb49a6c7
Sameh khasawneh <Same7khasawneh@gmail.com>
2015-04-24 21:23:41 +0300

    final touches remain

--- foundconf.php
+++ foundconf.php
@@ -1,20 +1,64 @@
 <?php
-include_once("includes/navbar-user.php");
+include_once("includes/navbar.php");
 require_once("includes/functions.php");
+require_once("includes/database.php");
+require_once("includes/functions.php");
+require_once("includes/topic.php");
+require_once("includes/conference.php");
 
-if(!isset($_POST["search"])){
+
+if(isset($_POST["submit"])){
+
+    if(isset($_POST["search1"])){
+        $search = $_POST["search1"];
+        $search = preg_replace("#[^0-9a-z]#i","##",$search);
+
+        $query  = "SELECT conference.confName, conference.confDate, conference.photoURL FROM conference ";
+        $query .=  "WHERE conference.confName LIKE '%$search%' ORDER BY  conference.confName;";
+        $sql = conference::find_by_sql($query);
+        $counter = 0;
+        $counter2 = 0;
+        $array = array();
+        foreach($sql as $s){
+            foreach($s as $key) {
+                if (isset($key)) {
+                    $array[$counter] = $key;
+                    $counter++;
+                }
+            }
+            $counter2++;
+        }
+    }else{
+        $search = $_POST["search2"];
+        $search = preg_replace("#[^0-9a-z]#i","##",$search);
+
+
+        $query = "SELECT conference.confName, conference.confDate, conference.photoURL, topic.topicName FROM conference
+           INNER JOIN conftopic ON conference.ID = conftopic.confID
+           INNER JOIN topic ON topic.ID = conftopic.topicID WHERE topic.topicName LIKE '%$search%'
+           ORDER BY  conference.confName;";
+        $sql = conference::find_by_sql($query);
+        $counter = 0;
+        $counter2 = 0;
+        $array = array();
+        foreach($sql as $s){
+            foreach($s as $key) {
+                if (isset($key)) {
+                    $array[$counter] = $key;
+                    $counter++;
+                }
+            }
+            $counter2++;
+        }
+
+    }
+
+}else{
+
     redirect_to("findconf.php");
 }
 
-//if($_GET["submit"]){
-//
-//
-//
-//}
-//else{
-//    redirect_to();
-//}
-//?>
+?>
 
 
 <!DOCTYPE html>
@@ -50,27 +94,36 @@ if(!isset($_POST["search"])){
     <h1> Found Conferences </h1>
     <h6>Those are the results of your search</h6>
 
+    <h3><?php
+       if($counter2 != 1){
+           echo $counter2 ." Conferences found";
+       }else{
+           echo $counter2 ." Conference found";
+       }
+        ?> </h3>
+
+    <?php
+    for($i = 0 ; $i<=$counter-1 ; $i+=3) {
+
+        ?>
     <div class="row"> <!--EACH ROW IS FOR EACH CONFERENCE FOUND-->
 
 
         <div class="panel panel-success">
-            <div class="panel panel-heading">
-               <h3 class="panel-title">Conference 1</h3>
-            </div>
+
+
             <div class="panel-body">
 
                 <div class="col-md-3">
-                <img src="https://www.eda.europa.eu/images/default-source/procurement-gateway/conference-2.jpg" class="img-thumbnail" >
+                    <a type="hidden" href="conference.php?id=<?php echo $array[$i]; ?>">
+                    <img src="<?php echo $array[$i + 2] ?>" class="img-thumbnail">
                 </div>
 
                 <div class="col-md-4">
 
-                    <span class="row" >
-
-                        <p>Conference Name: <span>  PSUT MINA ICT 2015</span> </p>
-                        <p>Date:    <span>  22/5/2015</span> </p>
-                        <p>Topics: <span>  PSUT MINA ICT 2015</span> </p>
-
+                    <span class="row">
+                        <p>Conference Name: <span><?php echo htmlentities($array[$i]) ?></span></p>
+                        <p>Date: <span><?php echo htmlentities($array[$i+1]) ?></span></p>
                     </span>
 
                 </div>
@@ -78,73 +131,15 @@ if(!isset($_POST["search"])){
 
             </div>
 
-        </div>
-
-
-    </div>
-
-    <div class="row"> <!--EACH ROW IS FOR EACH CONFERENCE FOUND-->
-
-
-        <div class="panel panel-danger">
-
-            <div class="panel-body">
-
-
-
-            </div>
 
         </div>
 
 
-    </div>
-
-    <div class="row"> <!--EACH ROW IS FOR EACH CONFERENCE FOUND-->
-
-
-        <div class="panel panel-danger">
-
-            <div class="panel-body">
-
-
-
-            </div>
-
-        </div>
-
 
     </div>
-
-    <div class="row"> <!--EACH ROW IS FOR EACH CONFERENCE FOUND-->
-
-
-        <div class="panel panel-danger">
-
-            <div class="panel-body">
-
-
-
-            </div>
-
-        </div>
-
-
-    </div>
-    <div class="row"> <!--EACH ROW IS FOR EACH CONFERENCE FOUND-->
-
-
-        <div class="panel panel-danger">
-
-            <div class="panel-body">
-
-
-
-            </div>
-
-        </div>
-
-
-    </div>
+    <?php
+    }
+    ?>
 
 </body>     <!--BOdy ENd-->
 
@@ -186,7 +181,7 @@ if(!isset($_POST["search"])){
                 <div class="row">
                     <div class="col-lg-12">
                         <ul class="nav nav-pills nav-justified">
-                            <li><a href="/">© 2013 Company Name.</a></li>
+                            <li><a href="/">Copyright <?php echo htmlentities("© ");  echo date("Y",time()); ?>  Company Name.</a></li>
                             <li><a href="#">Terms of Service</a></li>
                             <li><a href="#">Privacy</a></li>
                         </ul>
