dfc4d35750020e252269d24020ce259901a37fc6
Sameh khasawneh <Same7khasawneh@gmail.com>
2015-04-26 02:07:27 +0300

    final touches remain

--- includes/paper.php
+++ includes/paper.php
@@ -5,11 +5,12 @@ require_once("database.php");
 class paper extends DatabaseObject{
 
     protected static $table_name="paper";
-    protected static $db_fields=array('ID', 'userID', 'confID', 'dateSubmitted', 'paperURL', 'isAccepted');
+    protected static $db_fields=array('ID', 'userID', 'confID', 'paperName', 'dateSubmitted', 'paperURL', 'isAccepted');
 
     public $ID;
     public $userID;
     public $confID;
+    public $paperName;
     public $dateSubmitted;
     public $paperURL;
     public $isAccepted;
